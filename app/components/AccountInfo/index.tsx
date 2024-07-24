568aadfa133ee73835ed98a38eb24d469952df15
code-owen <owen1125@gmail.com>
2024-07-23 21:22:43 -0400

    Remove update from account page, add view calendar button

--- app/components/AccountInfo/index.tsx
+++ app/components/AccountInfo/index.tsx
@@ -1,126 +1,59 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Button from '../Button/index';
-import UpdateButton from '../UpdateButton/index';
 import { useSession } from 'next-auth/react';
+import { useRouter } from "next/navigation";
+import Button from "../Button";
 
 const Form = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [defaultFirstName, setDefaultFirstName] = useState('');
-  const [defaultLastName, setDefaultLastName] = useState('');
-  const [defaultEmail, setDefaultEmail] = useState('');
   const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
-    if (status === 'authenticated' && session?.user?.email) {
-      const fetchUser = async () => {
-        try { 
-          const email = session?.user?.email;
-          const response = await fetch('/api/get-user', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email }),
-          });
-          const data = await response.json();
-          if (response.ok) {
-            setFirstName(data.first_name);
-            setLastName(data.last_name);
-            setEmail(data.email);
-            setDefaultFirstName(data.first_name);
-            setDefaultLastName(data.last_name);
-            setDefaultEmail(data.email);
-            setError('');
-          } else {
-            setError(data.error);
-          }
-        } catch (err) {
-          console.error('Error fetching user data:', err);
-          setError('Failed to load user data.');
+    const fetchUser = async (email: string) => {
+      try {
+        const response = await fetch('/api/get-user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+          setFirstName(data.first_name);
+          setLastName(data.last_name);
+          setEmail(data.email);
+          setError('');
+        } else {
+          setError(data.error);
         }
-      };
+      } catch (err) {
+        console.error('Error fetching user data:', err);
+        setError('Failed to load user data.');
+      }
+    };
 
-      fetchUser();
+    if (status === 'authenticated' && session?.user?.email) {
+      fetchUser(session.user.email);
     }
   }, [status, session]);
 
-  const handleUpdate = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (
-      firstName === defaultFirstName &&
-      lastName === defaultLastName &&
-      email === defaultEmail
-    ) {
-      setError('No changes made to update.');
-      setSuccess('');
-      return;
-    }
-
-    try {
-      const response = await fetch('/api/update-user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: defaultEmail,
-          new_email: email,
-          first_name: firstName,
-          last_name: lastName,
-        }),
-      });
-      const data = await response.json();
-
-      if (response.ok) {
-        setFirstName(data.first_name);
-        setLastName(data.last_name);
-        setEmail(data.email);
-        setDefaultFirstName(data.first_name);
-        setDefaultLastName(data.last_name);
-        setDefaultEmail(data.email);
-        setError('');
-        setSuccess('Update Successful');
-
-        setTimeout(() => {
-          setSuccess('');
-        }, 5000);
-      } else {
-        setError(data.error);
-        setSuccess('');
-      }
-    } catch (err) {
-      console.error('Error updating user data:', err);
-      setError('Failed to update user data.');
-      setSuccess('');
-    }
-  };
-
-  const handleCancel = () => {
-    setFirstName(defaultFirstName);
-    setLastName(defaultLastName);
-    setEmail(defaultEmail);
-    setError('');
-    setSuccess('');
-  };
-
   return (
-    <form className="bg-white p-14 rounded-xl shadow-xl w-full max-w-xl" onSubmit={handleUpdate}>
-      {error && <div className="text-red-700 mb-4">Error: {error}</div>}
-      {success && <div className="text-green-700 mb-4">{success}</div>}
+    <form className="bg-white p-14 rounded-xl shadow-xl w-full max-w-xl">
+      {error && <div className="text-red-700 mb-4">{error}</div>}
       <div className="mb-4 flex space-x-10">
         <div className="w-1/2">
           <label className="block mb-2">First Name</label>
           <input
             type="text"
             value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            readOnly
             className="w-full px-3 py-2 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-accent bg-gradientColor1 text-black font-medium"
           />
         </div>
@@ -129,7 +62,7 @@ const Form = () => {
           <input
             type="text"
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            readOnly
             className="w-full px-3 py-2 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-accent bg-gradientColor1 text-black font-medium"
           />
         </div>
@@ -139,13 +72,18 @@ const Form = () => {
         <input
           type="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          readOnly
           className="w-full px-3 py-2 mb-8 border border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-accent bg-gradientColor1 text-black font-medium"
         />
       </div>
       <div className="flex justify-start">
-        <Button buttonText={'Cancel'} buttonClassName={'bg-gradientColor1 text-accent py-2 px-4 rounded mr-2 drop-shadow-lg'} onClick={handleCancel} />
-        <UpdateButton buttonText={'Update'} buttonClassName={'bg-accent text-white py-2 px-4 rounded mr-2 drop-shadow-lg'} onClick={handleUpdate} />
+        <Button
+          buttonText={'View Calendar'}
+          buttonClassName={'bg-accent text-white py-2 px-4 rounded mr-2 drop-shadow-lg'}
+          onClick={() => {
+            router.push("/view-calendar");
+          }}
+        />
       </div>
     </form>
   );
