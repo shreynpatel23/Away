284877542c45b1809d49013f5777e8de8d0745b3
code-owen <owen1125@gmail.com>
2024-07-26 14:49:02 -0400

    Remove updateButton

--- app/components/UpdateButton/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IUpdateButtonProps {
-    buttonText: string;
-    buttonClassName: string;
-    onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    hasIcon?: boolean;
-    icon?: React.ReactNode;
-    isDisabled?: boolean;
-    isLoading?: boolean;
-  }
