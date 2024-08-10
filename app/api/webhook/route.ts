a6013e9c31473dc935f41bf39aaaaf0a0c634e95
codeyumm <pvashi006@gmail.com>
2024-08-10 11:35:06 -0400

    Added meta data in stripe payment to save data in db

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
-import mongoose from 'mongoose';
-import User from '@/models/User'; 
-import ConnectDB from '@/lib/mongodb';
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import mongoose from "mongoose";
+import User from "@/models/User";
+import ConnectDB from "@/lib/mongodb";
 
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY!);
 
@@ -15,50 +15,44 @@ export const config = {
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
 export const POST = async (req: NextRequest) => {
+  const buf = Buffer.from(await req.arrayBuffer());
+  const sig = req.headers.get("stripe-signature")!;
 
-    const buf = Buffer.from(await req.arrayBuffer());
-    const sig = req.headers.get('stripe-signature')!;
+  let event: Stripe.Event;
 
-    let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+  } catch (err) {
+    return new NextResponse("Webhook Error: ", { status: 400 });
+  }
 
-    try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-    } catch (err) {
-      return new NextResponse('Webhook Error: ', { status: 400 });
-    }
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object as Stripe.Checkout.Session;
 
-    // Handle the event
-    switch (event.type) {
-      
-      case 'checkout.session.completed':
-        const session =  event.data.object as Stripe.Checkout.Session;
+      const email = session.metadata?.userId;
+      const planType = session.metadata?.planType;
 
-
-
-        const email = session.metadata?.userId;
-        const planType = session.metadata?.planType;
-
-        if (email && planType) {
-          
-          try {
-            await ConnectDB();
-            await User.findOneAndUpdate(
-              { email: email }, 
-              {
-                isPaidUser: true,
-                planType: planType,
-              }
-            );
-          } catch (dbErr) {
-            
-            return new NextResponse('Database update failed', { status: 500 });
-          }
+      if (email && planType) {
+        try {
+          await ConnectDB();
+          await User.findOneAndUpdate(
+            { email: email },
+            {
+              isPaidUser: true,
+              planType: planType,
+            }
+          );
+        } catch (dbErr) {
+          return new NextResponse("Database update failed", { status: 500 });
         }
-        break;
+      }
+      break;
 
-      default:
-        console.log(`Unhandled event type ${event.type}`);
+    default:
+      console.log(`Unhandled event type ${event.type}`);
 
-    return new NextResponse('Received', { status: 200 });
-  } 
+      return new NextResponse("Received", { status: 200 });
+  }
 };
