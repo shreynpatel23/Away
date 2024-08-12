b86e576dbe1c71ef76ce08108ef0d653371fee80
code-owen <owen1125@gmail.com>
2024-08-12 13:19:44 -0400

    Remove bannerContext from Banner component

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import Button from "../Button";
-import {
-  BannerContextProvider,
-  useBannerContext,
-} from "@/app/context/bannerContext";
 
-function BannerContent() {
-  const { isPaidUser } = useBannerContext();
-
-  if (isPaidUser) {
-    return null;
-  }
-
+export default function Banner() {
   return (
     <div className="my-4 w-full flex items-center justify-center gap-8 p-6 bg-accent border border-accent rounded-[16px]">
       <p className="text-base text-white">
@@ -26,13 +16,5 @@ function BannerContent() {
         onClick={() => console.log("upgrade now clicked!")}
       />
     </div>
-  );
-}
-
-export default function Banner() {
-  return (
-    <BannerContextProvider>
-      <BannerContent />
-    </BannerContextProvider>
   );
 }
